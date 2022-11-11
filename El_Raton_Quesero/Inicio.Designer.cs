9017e93b208c9373f1430460adb4beb60fdc7cc7
EduardoVarela0144 <014426634@ulsaoaxaca.edu.mx>
2022-11-11 14:23:59 -0600

    first commit

---
+++ El_Raton_Quesero/Inicio.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace El_Raton_Quesero
+{
+    partial class Juego
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Juego));
+            this.player = new System.Windows.Forms.PictureBox();
+            this.Puntaje = new System.Windows.Forms.Label();
+            this.mainTimer = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnReanudar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nT = new System.Windows.Forms.NumericUpDown();
+            this.nQ = new System.Windows.Forms.NumericUpDown();
+            this.btnReiniciar = new System.Windows.Forms.Button();
+            this.Pausar = new System.Windows.Forms.Button();
+            this.btnEmpezar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nT)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nQ)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("player.BackgroundImage")));
+            this.player.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.player.Location = new System.Drawing.Point(333, 297);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(86, 83);
+            this.player.TabIndex = 5;
+            this.player.TabStop = false;
+            this.player.Visible = false;
+            // 
+            // Puntaje
+            // 
+            this.Puntaje.AutoSize = true;
+            this.Puntaje.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.Puntaje.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Puntaje.Location = new System.Drawing.Point(30, 168);
+            this.Puntaje.Name = "Puntaje";
+            this.Puntaje.Size = new System.Drawing.Size(34, 31);
+            this.Puntaje.TabIndex = 6;
+            this.Puntaje.Text = "--";
+            this.Puntaje.Visible = false;
+            // 
+            // mainTimer
+            // 
+            this.mainTimer.Interval = 20;
+            this.mainTimer.Tick += new System.EventHandler(this.mainTimerEvent);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.Controls.Add(this.btnReanudar);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.nT);
+            this.panel1.Controls.Add(this.nQ);
+            this.panel1.Controls.Add(this.btnReiniciar);
+            this.panel1.Controls.Add(this.Pausar);
+            this.panel1.Controls.Add(this.btnEmpezar);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-5, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(780, 152);
+            this.panel1.TabIndex = 7;
+            // 
+            // btnReanudar
+            // 
+            this.btnReanudar.Enabled = false;
+            this.btnReanudar.Location = new System.Drawing.Point(550, 102);
+            this.btnReanudar.Name = "btnReanudar";
+            this.btnReanudar.Size = new System.Drawing.Size(75, 23);
+            this.btnReanudar.TabIndex = 8;
+            this.btnReanudar.Text = "Reanudar";
+            this.btnReanudar.UseVisualStyleBackColor = true;
+            this.btnReanudar.Click += new System.EventHandler(this.btnReanudar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("SquareFont", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(548, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 11);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Trampas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("SquareFont", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(454, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 11);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "QUESOS";
+            // 
+            // nT
+            // 
+            this.nT.Location = new System.Drawing.Point(550, 76);
+            this.nT.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.nT.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nT.Name = "nT";
+            this.nT.Size = new System.Drawing.Size(75, 20);
+            this.nT.TabIndex = 5;
+            this.nT.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // nQ
+            // 
+            this.nQ.Location = new System.Drawing.Point(457, 76);
+            this.nQ.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.nQ.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nQ.Name = "nQ";
+            this.nQ.Size = new System.Drawing.Size(75, 20);
+            this.nQ.TabIndex = 4;
+            this.nQ.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // btnReiniciar
+            // 
+            this.btnReiniciar.Enabled = false;
+            this.btnReiniciar.Location = new System.Drawing.Point(457, 102);
+            this.btnReiniciar.Name = "btnReiniciar";
+            this.btnReiniciar.Size = new System.Drawing.Size(75, 23);
+            this.btnReiniciar.TabIndex = 3;
+            this.btnReiniciar.Text = "Reiniciar";
+            this.btnReiniciar.UseVisualStyleBackColor = true;
+            this.btnReiniciar.Click += new System.EventHandler(this.btnReiniciar_Click);
+            // 
+            // Pausar
+            // 
+            this.Pausar.Enabled = false;
+            this.Pausar.Location = new System.Drawing.Point(638, 102);
+            this.Pausar.Name = "Pausar";
+            this.Pausar.Size = new System.Drawing.Size(75, 23);
+            this.Pausar.TabIndex = 2;
+            this.Pausar.Text = "Pausar";
+            this.Pausar.UseVisualStyleBackColor = true;
+            this.Pausar.Click += new System.EventHandler(this.Pausar_Click);
+            // 
+            // btnEmpezar
+            // 
+            this.btnEmpezar.Location = new System.Drawing.Point(638, 73);
+            this.btnEmpezar.Name = "btnEmpezar";
+            this.btnEmpezar.Size = new System.Drawing.Size(75, 23);
+            this.btnEmpezar.TabIndex = 1;
+            this.btnEmpezar.Text = "Empezar";
+            this.btnEmpezar.UseVisualStyleBackColor = true;
+            this.btnEmpezar.Click += new System.EventHandler(this.btnEmpezar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("SquareFont", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(452, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(281, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "EL RATÓN QUESERO";
+            // 
+            // Juego
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(749, 493);
+            this.Controls.Add(this.Puntaje);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Juego";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "El ratón quesero";
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nT)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nQ)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Label Puntaje;
+        private System.Windows.Forms.Timer mainTimer;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEmpezar;
+        private System.Windows.Forms.Button Pausar;
+        private System.Windows.Forms.NumericUpDown nT;
+        private System.Windows.Forms.NumericUpDown nQ;
+        private System.Windows.Forms.Button btnReiniciar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnReanudar;
+    }
+}
+
---
+++ El_Raton_Quesero/Inicio.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Drawing;
+using System.Media;
+using System.Windows.Forms;
+
+
+namespace El_Raton_Quesero
+{
+    
+    public partial class Juego : Form
+    {
+        
+        int LimiteX = 675;
+        int LimiteY = 442;
+        int vx = 5, vy = 5;
+        int cvidas = 0;
+
+        Random rnd = new Random();
+
+        /*private void S()
+        {
+            var sonido2 = new SoundPlayer(El_Raton_Quesero.Properties.Resources.GS);
+            sonido2.Play();
+        }*/
+
+        /*private void Fondo()
+        {
+            var sonido2 = new SoundPlayer(El_Raton_Quesero.Properties.Resources.Fondo);
+            sonido2.Play();
+        }*/
+
+        public Juego()
+        {
+            InitializeComponent();
+            //Fondo();
+        }
+
+        
+
+        private void btnEmpezar_Click(object sender, EventArgs e)
+        {
+            //S();
+            player.Visible = true;
+            Puntaje.Visible = true;
+            mainTimer.Start();
+            btnEmpezar.Enabled = false;
+            btnReiniciar.Enabled = true;
+            Pausar.Enabled = true;
+            nQ.Enabled = false;
+            nT.Enabled = false;
+            btnReanudar.Enabled = false;
+
+            int Quesos = (int)nQ.Value;
+            int Trampas = (int)nT.Value;
+            Image imag = El_Raton_Quesero.Properties.Resources.Vidas;
+            Image imag2 = El_Raton_Quesero.Properties.Resources.Queso;
+            Image imag3 = El_Raton_Quesero.Properties.Resources.Trampa;
+            for (int i = 0; i < Quesos; i++)
+            {
+             int x = rnd.Next(0, 179);
+             int y = rnd.Next(205, 416);
+                var picture = new PictureBox
+            {
+                Name = "Queso"+(i+1),
+                Size = new Size(50, 50),
+                Location = new Point(x, y),
+                Tag = "object",
+                BackColor = Color.Transparent,
+                BackgroundImage = imag2,
+                BackgroundImageLayout = ImageLayout.Stretch
+            };
+                this.Controls.Add(picture);
+            }
+
+            for (int i = 0; i < Trampas; i++)
+            {
+                int a = rnd.Next(0, 179);
+                int b = rnd.Next(205, 416);
+                var picture = new PictureBox
+                {
+                    Name = "Trampas" +(1+i),
+                    Size = new Size(50, 50),
+                    Location = new Point(a, b),
+                    Tag = "object",
+                    BackColor = Color.Transparent,
+                    BackgroundImage = imag3,
+                    BackgroundImageLayout = ImageLayout.Stretch
+                };
+                this.Controls.Add(picture);
+            }
+            int contador = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                
+                contador = contador + 31;
+                var picture = new PictureBox
+                {
+                    
+                    Name = "Vida" + (1 + i),
+                    Size = new Size(25, 25),
+                    Location = new Point(619+contador, 158),
+                    BackColor = Color.Transparent,
+                    BackgroundImage = imag,
+                    BackgroundImageLayout = ImageLayout.Stretch
+                };
+                this.Controls.Add(picture);
+            }
+
+        }
+
+        private void Pausar_Click(object sender, EventArgs e)
+        {
+            mainTimer.Stop();
+            btnEmpezar.Enabled = false;
+            btnReanudar.Enabled = true;
+
+        }
+
+        private void btnReiniciar_Click(object sender, EventArgs e)
+        {
+            Puntaje.Text = "--";
+            btnReanudar.Enabled = false;
+            Pausar.Enabled = true;
+            mainTimer.Stop();
+            foreach (Control x in this.Controls)
+            {
+                int d = rnd.Next(0, 179);
+                int f = rnd.Next(205, 416);
+                if (x is PictureBox && (string)x.Tag == "object")
+                {
+                    x.Location = new Point(d, f);
+                }
+            }
+            player.Location = new Point(333, 297);
+            mainTimer.Start();
+            btnEmpezar.Enabled = false;
+            //Fondo();
+            cvidas = 0;
+            foreach (Control z in this.Controls)
+            {
+                if (z is PictureBox && z.Name.Contains("Vida"))
+                {
+                    z.Visible = true;
+                }
+            }
+        }
+
+        private void btnReanudar_Click(object sender, EventArgs e)
+        {
+            btnEmpezar.Enabled = false;
+            mainTimer.Enabled = true;
+        }
+
+        private void mainTimerEvent(object sender, EventArgs e)
+        {
+            
+            player.Left = player.Left + vx;
+            player.Top = player.Top + vy;
+
+            // Colisión con bordes
+            if (player.Left > LimiteX || player.Left <= 0)
+            {
+                vx *= -1;       // Cambiar dirección
+            }
+
+            if (player.Top > LimiteY || player.Top <= 153)
+            {
+                vy *= -1;       // Cambiar dirección
+            }
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "object")
+                {
+                    
+                        if (player.Bounds.IntersectsWith(x.Bounds))
+                        {
+                            Puntaje.Text = "Objeto - " + x.Name;
+                            if (x.Name.Contains("Queso"))
+                                {
+                                x.BackColor = Color.Green;
+                                //S();
+                                }
+                                if (x.Name.Contains("Trampa"))
+                                {
+                                    x.BackColor = Color.Red;
+                                    cvidas = cvidas + 1;
+                                    foreach (Control z in this.Controls)
+                                    {
+                                        if (z is PictureBox && z.Name.Contains("Vida"+cvidas))
+                                        {
+                                            z.Visible = false;
+                                        }
+                                    }
+                                    if (cvidas==3)
+                                        {
+                                        mainTimer.Stop();
+                                        MessageBox.Show("Perdiste");
+                                        btnEmpezar.Enabled = false;
+                                        Pausar.Enabled = false;
+                                        btnReiniciar.Enabled = true;
+                                        }
+                                }
+
+                        
+                            vx *= -1;
+                        }
+                        else {
+                        x.BackColor = Color.Transparent;
+                        } 
+                }
+                
+            }
+        }
+        
+    }
+
+    internal struct NewStruct
+    {
+        public object Item1;
+        public object Item2;
+
+        public NewStruct(object item1, object item2)
+        {
+            Item1 = item1;
+            Item2 = item2;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is NewStruct other &&
+                   System.Collections.Generic.EqualityComparer<object>.Default.Equals(Item1, other.Item1) &&
+                   System.Collections.Generic.EqualityComparer<object>.Default.Equals(Item2, other.Item2);
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = -1030903623;
+            hashCode = hashCode * -1521134295 + System.Collections.Generic.EqualityComparer<object>.Default.GetHashCode(Item1);
+            hashCode = hashCode * -1521134295 + System.Collections.Generic.EqualityComparer<object>.Default.GetHashCode(Item2);
+            return hashCode;
+        }
+
+        public void Deconstruct(out object item1, out object item2)
+        {
+            item1 = Item1;
+            item2 = Item2;
+        }
+
+        public static implicit operator (object, object)(NewStruct value)
+        {
+            return (value.Item1, value.Item2);
+        }
+
+        public static implicit operator NewStruct((object, object) value)
+        {
+            return new NewStruct(value.Item1, value.Item2);
+        }
+    }
+}
